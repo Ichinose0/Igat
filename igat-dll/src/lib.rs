3c99b4bfd28b230ceaa6e84779deb393e8487813
Ichinose0 <148521661+Ichinose0@users.noreply.github.com>
2023-12-22 08:30:36 +0900

    Defined some ffi types

--- igat-dll/src/lib.rs
+++ igat-dll/src/lib.rs
@@ -1,4 +1,52 @@
 #![no_main]
 
+use std::ptr::{null,null_mut};
+
+pub type MESSEAGEPROCPTR = fn();
+pub type UIPROCPTR = fn();
+pub type SETUPPROCPTR = fn();
+pub type THEMEPROCPTR = fn(theme: Theme);
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct Rect {
+    pub left: u32,
+    pub top: u32,
+    pub right: u32,
+    pub bottom: u32,
+}
+
+#[repr(C)]
+pub enum Menubar {
+
+}
+
+#[repr(C)]
+pub enum Menu {
+    
+}
+
+#[repr(C)]
+pub enum Theme {
+
+}
+
+#[repr(C)]
+pub enum Frame {
+
+}
+
+#[repr(C)]
+pub enum Application {
+
+}
+
+pub struct DefaultApplication {
+    ui: UIPROCPTR
+}
+
+
 #[no_mangle]
-pub extern "C" fn InitIgat() {}
+pub extern "C" fn CreateApplication() -> *mut Application {
+    null_mut();
+}
